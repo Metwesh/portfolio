46ae4aa5d090168c485f7fb82f1c7dff9bc0ee47
Mohamed H. Aly <mohamedh.aly@hotmail.com>
2025-07-16 00:19:36 +0300

    Refactor base path handling in Vite config for better clarity and consistency

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 const getBasePath = (mode: string) =>
-  mode === "development" ? "/" : "/portfolio/";
+  mode === "development" ? undefined : "/portfolio/";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? undefined : "/portfolio/",
+  base: getBasePath(mode),
   plugins: [
     react(),
     tailwindcss(),
@@ -33,28 +33,28 @@ export default defineConfig(({ mode }) => ({
         scope: "/",
         icons: [
           {
-            src: `${getBasePath(mode)}favicons/pwa-64x64.png`,
+            src: `${getBasePath(mode) ?? "/"}favicons/pwa-64x64.png`,
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: `${getBasePath(mode)}favicons/pwa-192x192.png`,
+            src: `${getBasePath(mode) ?? "/"}favicons/pwa-192x192.png`,
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: `${getBasePath(mode)}favicons/pwa-512x512.png`,
+            src: `${getBasePath(mode) ?? "/"}favicons/pwa-512x512.png`,
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: `${getBasePath(mode)}favicons/pwa-512x512.png`,
+            src: `${getBasePath(mode) ?? "/"}favicons/pwa-512x512.png`,
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: `${getBasePath(mode)}favicons/maskable-icon-512x512.png`,
+            src: `${getBasePath(mode) ?? "/"}favicons/maskable-icon-512x512.png`,
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
